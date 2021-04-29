8a2583ec94cb168d7d9c04dadfa7b41114ac133e
ewa <ewapasternak.kontakt@gmail.com>
2021-04-29 14:30:37 +0200

    Add for for adding new people

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [person, setPerson] = useState({});
+  const [people, setPeople] = useState([{ name: "Jan", lastName: "Kowalski" }]);
+
+  const handleChange = (key) => (event) => {
+    setPerson({ ...person, [key]: event.target.value });
+  };
+
+  const handleAddPerson = (event) => {
+    event.preventDefault();
+    setPeople([...people, person]);
+  };
+
   return (
     <div className="App">
       <header className="A">A</header>
@@ -10,14 +23,30 @@ function App() {
           C<div className="E">E</div>
         </div>
       </div>
+      <form className="form" onSubmit={handleAddPerson}>
+        <div className="form-name">
+          <label>Imię</label>
+          <input required type="text" onChange={handleChange("name")} />
+        </div>
+        <div className="form-lastname">
+          <label>Nazwisko</label>
+          <input type="text" onChange={handleChange("lastName")} />
+        </div>
+        <button type="submit">Add</button>
+      </form>
       <div className="F">
         <p className="F-title">Pracownicy</p>
-        <p className="F-name">Jan</p>
-        <p className="F-surname">Kowalski</p>F
+        {people.map(({ name, lastName }) => (
+          <>
+            <p className="F-name">{name}</p>
+            <p className="F-lastName">{lastName}</p>
+          </>
+        ))}
+        F
       </div>
       <footer className="D">D</footer>
     </div>
   );
-}
+};
 
 export default App;
